390a8314709e54d4619d0a17f95c9cfd81b99b1a
RistainoGiuseppe <giuseppe.ristaino@gmail.com>
2026-02-16 00:22:50 +0100

    config production git hub

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/developer_page/" : "/", // <--- aggiungi questa riga
   server: {
     host: "::",
     port: 8080,
