6209fb49eaa71cb5ab34368c16a994be594f93a2
RistainoGiuseppe <giuseppe.ristaino@gmail.com>
2026-06-06 00:31:07 +0200

    Deploy to root domain giusepperistaino.github.io

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/developer_page/" : "/", // <--- aggiungi questa riga
+  base: "/",
   server: {
     host: "::",
     port: 8080,
